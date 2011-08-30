0491840bb04faf40098b3a0e2d6c8be692c39c76
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-08-30 17:53:46 +0200

    [UPDATE] Update mysql indexation setup

--- setup/initData.php
+++ setup/initData.php
@@ -6,6 +6,7 @@ class mysqlindexer_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
+		$this->addProjectConfigurationEntry('injection/class/indexer_IndexService', 'mysqlindexer_IndexService');
 	}
 
 	/**
