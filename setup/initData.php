62e7d609ff29d374e4d320ded70c4db93f2e93db
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:44:50 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -8,16 +8,4 @@ class mysqlindexer_Setup extends object_InitDataSetup
 	{
 		$this->addProjectConfigurationEntry('injection/class/indexer_IndexService', 'mysqlindexer_IndexService');
 	}
-
-	/**
-	 * @return string[]
-	 */
-	public function getRequiredPackages()
-	{
-		// Return an array of packages name if the data you are inserting in
-		// this file depend on the data of other packages.
-		// Example:
-		// return array('modules_website', 'modules_users');
-		return array();
-	}
 }
