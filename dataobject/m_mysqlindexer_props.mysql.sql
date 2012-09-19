cabded5e4713b59ae6dd226235a7136937be0d9a
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-09-19 12:59:12 +0200

    [UPDATE] change mysql tables CHARACTER

--- dataobject/m_mysqlindexer_props.mysql.sql
+++ dataobject/m_mysqlindexer_props.mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `m_mysqlindexer_props` (
-	`final_id` varchar(50) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-	`prop` varchar(100) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-	`data` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+	`final_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`prop` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+	`data` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
 	UNIQUE KEY `PROP` (`final_id`,`prop`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
